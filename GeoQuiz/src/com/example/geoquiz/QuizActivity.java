ccd0fb1d9bb487a2ccf50fe87e6e9d7b9c1af3ac
rdatu <r.datu@klab.com>
2015-06-29 16:13:43 +0800

    Applying MVC in GeoQuiz

--- GeoQuiz/src/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/example/geoquiz/QuizActivity.java
@@ -5,35 +5,65 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true), };
+
+	private int mCurrentIndex = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
 		mFalseButton = (Button) findViewById(R.id.false_button);
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(false);
+			}
+		});
+		
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				UpdateQuestion();			
 			}
 		});
+		
+		UpdateQuestion();
 	}
 
 	@Override
@@ -41,5 +71,24 @@ public class QuizActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
+	}
+	
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		if(userPressedTrue == answerIsTrue){
+			messageResId = R.string.correct_toast;
+		}
+		else{
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+	
+	private void UpdateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
 	}
 }
